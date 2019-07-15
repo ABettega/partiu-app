ac42cb9331a8aa3c6b792a0a5fce5ad8ea7e1094
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-07-15 11:43:31 -0300

    basic setup

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser'); 
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongoDB');
+});
+
+app.use(morgan('dev'));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+app.listen(process.env.PORT, () => console.log(`server is running on port ${process.env.PORT}`));
