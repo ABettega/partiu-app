b51820894d9d7bb19283c7efb6ce6d2ddf53df42
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-07-19 11:16:55 -0300

    initial test

--- public/js/script.js
+++ public/js/script.js
@@ -1,44 +1,17 @@
-let buttons = document.getElementsByClassName('btnEventoSalvar');
-for(let i = 0; i < buttons.length; i += 1) {
-  let button = buttons[i];
-  button.addEventListener('click', function() {
-    let id = button.id.slice(button.id.length-1, button.id.length);
-    let hotelLink = document.getElementById(`hotelLink${id}`).getAttribute('value');
-    let hotelFoto = document.getElementById(`hotelFoto${id}`).getAttribute('value');
-    let hotelNome = document.getElementById(`hotelNome${id}`).getAttribute('value');
-    let hotelCidade = document.getElementById(`hotelCidade${id}`).getAttribute('value');
-    let hotelPreco = document.getElementById(`hotelPreco${id}`).getAttribute('value');
-    let hotelEstrelas = document.getElementById(`hotelEstrelas${id}`).getAttribute('value');
-    let hotelRating = document.getElementById(`hotelRating${id}`).getAttribute('value');
-    let hotelReviews = document.getElementById(`hotelReviews${id}`).getAttribute('value');
-    let itinCusto = document.getElementById(`itinCusto${id}`).getAttribute('value');
-    let itinDiaIda = document.getElementById(`itinDiaIda${id}`).getAttribute('value');
-    let itinDiaVolta = document.getElementById(`itinDiaVolta${id}`).getAttribute('value');
-    let itinOrigem = document.getElementById(`itinOrigem${id}`).getAttribute('value');
-    let itinDestino = document.getElementById(`itinDestino${id}`).getAttribute('value');
+let moedas = document.getElementsByClassName('currencySelector');
 
-    let hotel = {
-      hotelNome,
-      hotelCidade,
-      hotelPreco,
-      hotelEstrelas,
-      hotelRating,
-      hotelReviews,
-      hotelFoto,
-      hotelLink
-    };
-    let post = {
-      itinCusto,
-      itinDiaIda,
-      itinDiaVolta,
-      itinOrigem,
-      itinDestino,
-      hotel
-    }
-    
-    axios.post('/main/favorite', post)
-      .then(() => {})
-      .catch(err => console.log(err))
-
-  })
+function k(i) {
+	var v = i.value.replace(/\D/g,'');
+	v = (v/100).toFixed(2) + '';
+	// v = v.replace(".", ",");
+	v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+	v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+	i.value = v;
 }
+
+for(let i = 0; i < moedas.length; i += 1) {
+  let campoMoeda = moedas[i];
+  let valor = k(campoMoeda.getAttribute('value'));
+  campoMoeda.setAttribute('value', valor);
+}
+
